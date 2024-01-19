b83eae330eb6927a5b15f42a46ab9507c30d4fef
zetsub0 <g.echilov@gmail.com>
2024-01-19 14:38:44 +0300

    added links cache

--- internal/stringToLink/stringToLink.go
+++ internal/stringToLink/stringToLink.go
@@ -2,7 +2,23 @@ package stringToLink
 
 import (
 	"strings"
+	"sync"
+	"time"
 )
+
+var (
+	cacheMutex sync.Mutex
+	cache      map[string]cacheEntry
+)
+
+type cacheEntry struct {
+	links     map[string]string
+	timestamp time.Time
+}
+
+func init() {
+	cache = make(map[string]cacheEntry)
+}
 
 var (
 	ozon       = "Ozon"
@@ -16,21 +32,30 @@ var (
 )
 
 func StringToLink(bookName string) map[string]string {
-	res := make(map[string]string)
+	cacheMutex.Lock()
+	defer cacheMutex.Unlock()
+
+	if result, found := cache[bookName]; found {
+		if time.Since(result.timestamp).Minutes() < 60 {
+			return result.links
+		}
+	}
+
+	links := make(map[string]string)
 	// word1+word2+word3
 	plusString := strings.Join(strings.Split(bookName, " "), "+")
 	// word1%20word2%20word3
 	percString := strings.Join(strings.Split(bookName, " "), "%20")
 
-	res[ozon] = "https://www.ozon.ru/category/knigi-16500/?category_was_predicted=true&from_global=true&text=" + plusString
-	res[avito] = "https://www.avito.ru/all/knigi_i_zhurnaly?cd=1&q=" + plusString
-	res[piter] = "https://www.piter.com/collection/all?q=" + plusString
-	res[dmk] = "https://dmkpress.com/search/product/?search_string=" + plusString
+	links[ozon] = "https://www.ozon.ru/category/knigi-16500/?category_was_predicted=true&from_global=true&text=" + plusString
+	links[avito] = "https://www.avito.ru/all/knigi_i_zhurnaly?cd=1&q=" + plusString
+	links[piter] = "https://www.piter.com/collection/all?q=" + plusString
+	links[dmk] = "https://dmkpress.com/search/product/?search_string=" + plusString
 
-	res[chitaiG] = "https://www.chitai-gorod.ru/search?phrase=" + percString
-	res[wb] = "https://www.wildberries.ru/catalog/0/search.aspx?search=" + percString
-	res[marketYa] = "https://market.yandex.ru/catalog--knigi/54510/list?text=" + percString
-	res[megaMarket] = "https://megamarket.ru/catalog/?q=" + percString
+	links[chitaiG] = "https://www.chitai-gorod.ru/search?phrase=" + percString
+	links[wb] = "https://www.wildberries.ru/catalog/0/search.aspx?search=" + percString
+	links[marketYa] = "https://market.yandex.ru/catalog--knigi/54510/list?text=" + percString
+	links[megaMarket] = "https://megamarket.ru/catalog/?q=" + percString
 
-	return res
+	return links
 }
